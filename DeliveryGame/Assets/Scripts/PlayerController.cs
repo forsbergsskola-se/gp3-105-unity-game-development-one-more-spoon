d074ac22d75a17a20da3856e52ec52e19cadbb02
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-17 15:58:55 +0200

    PlayerMovmentCodeCreated

--- DeliveryGame/Assets/Scripts/PlayerController.cs
+++ DeliveryGame/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float movementSpeed;
     public int health = 3;
     // Start is called before the first frame update
     void Start()
@@ -15,19 +17,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       PlayerMovement();
+       
     }
-
+       void PlayerMovement()
+        {
+             Rigidbody rigidbody = GetComponent<Rigidbody>();
+                    
+                    if(Input.GetKey(KeyCode.W)) {
+                        transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+                    }
+                    else if(Input.GetKey(KeyCode.S)) {
+                        rigidbody.position += Vector3.back * Time.deltaTime * movementSpeed;
+                    }
+                    else if(Input.GetKey(KeyCode.A)) {
+                        rigidbody.position += Vector3.left * Time.deltaTime * movementSpeed;
+                    }
+                    else if(Input.GetKey(KeyCode.D)) {
+                        rigidbody.position += Vector3.right * Time.deltaTime * movementSpeed;
+                    }
+        }
     public void healthRemover()
     {
         health -= health;
     }
     
     
-    void PlayerJump()
-    {
-        
-    }
+
 
     
 }
