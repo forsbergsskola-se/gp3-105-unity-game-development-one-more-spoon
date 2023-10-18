e2a6565646b0bce3aca39b4f0ef9d092de6dec6e
sputgarin <sputgarin@protonmail.com>
2023-10-18 16:39:02 +0200

    Created the camera script

--- DeliveryGame/Assets/Scripts/CameraScript.cs
+++ DeliveryGame/Assets/Scripts/CameraScript.cs
@@ -1,30 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
-    public float turnSpeed = 5.0f;
     public GameObject player;
-
-    private Transform playerTransform;     
-    private Vector3 offset;
-    private float yOffset = 10.0f;
-    private float zOffset = 10.0f;
-
-    void Start () {
-        playerTransform = player.transform;
-        offset = new Vector3(playerTransform.position.x, playerTransform.position.y + yOffset, playerTransform.position.z + zOffset);
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    void FixedUpdate()
+    public Vector3 cameraOffset;
+    public Vector3 cameraRotationOffset;
+    private void LateUpdate()
     {
-        offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        transform.position = playerTransform.position + offset; 
-        transform.LookAt(playerTransform.position);
-    }  
+        
+        this.transform.position = player.transform.position + player.transform.TransformDirection(this.cameraOffset);
+        this.transform.rotation = player.transform.rotation * Quaternion.Euler(this.cameraRotationOffset);
+
+
+    }
 }
