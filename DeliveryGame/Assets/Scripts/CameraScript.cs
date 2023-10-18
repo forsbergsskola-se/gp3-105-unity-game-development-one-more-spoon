1ca2b073afc6d51cc99092eff20bd5cc7588f024
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-18 14:16:36 +0200

    CameraRotates

--- DeliveryGame/Assets/Scripts/CameraScript.cs
+++ DeliveryGame/Assets/Scripts/CameraScript.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public float turnSpeed = 5.0f;
+    public GameObject player;
+
+    private Transform playerTransform;     
+    private Vector3 offset;
+    private float yOffset = 10.0f;
+    private float zOffset = 10.0f;
+
+    void Start () {
+        playerTransform = player.transform;
+        offset = new Vector3(playerTransform.position.x, playerTransform.position.y + yOffset, playerTransform.position.z + zOffset);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
+        offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = playerTransform.position + offset; 
+        transform.LookAt(playerTransform.position);
+    }  
 }
