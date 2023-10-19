0048d7b1964700f4e2e724564b161b8ca4135eaa
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-10-19 13:02:46 +0200

    Added handout quest

---
+++ DeliveryGame/Assets/Scripts/HandoutQuest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandoutQuest : MonoBehaviour
+{
+    public int handoutItems = 5;
+    public GameObject missionText;
+
+}
