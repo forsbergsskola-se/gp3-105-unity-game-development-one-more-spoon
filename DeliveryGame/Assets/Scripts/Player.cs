4943932478ed0413368750519e6392a0c84a341a
sputgarin <sputgarin@protonmail.com>
2023-10-20 15:02:41 +0200

    Added so that when health is 0 you die

--- DeliveryGame/Assets/Scripts/Player.cs
+++ DeliveryGame/Assets/Scripts/Player.cs
@@ -8,13 +8,18 @@ using TMPro;
 public class Player : MonoBehaviour
 {
     public int health = 100;
-    public int score;
-    public int cash;
+    public int score = 0;
+    public int cash = 100;
     public bool isDying = false;
     
     public Vector3 startingPosition;
     public Vector3 startingRotation;
+
+    public TMP_Text healthText;
+    public TMP_Text scoreText;
     public TMP_Text cashText;
+    
+    
     public GameObject wastedText;
     
     private void Start()
@@ -26,7 +31,15 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        healthText.SetText(health.ToString());
+        scoreText.SetText(score.ToString());
         cashText.SetText(cash.ToString());
+        if (health <= 0 && isDying == false)
+        {
+            StartCoroutine(CheckHealth());
+        }
+        
+
     }
 
     public void HealPlayer(int healthAdded)
@@ -61,13 +74,14 @@ public class Player : MonoBehaviour
 
     public void Respawn()
     {
+        
         this.transform.rotation = Quaternion.Euler(startingRotation);
         this.transform.position = startingPosition;
         this.gameObject.GetComponent<PlayerController>().SetBackToIdle();
         this.gameObject.GetComponent<PlayerController>().movementSpeed = 4;
         this.gameObject.GetComponent<PlayerController>().rotationSpeed = 100;
         isDying = false;
-        
+        health = 100;
 
     }
 
@@ -92,4 +106,16 @@ public class Player : MonoBehaviour
         // Respawn
         Debug.Log("You died!");
     }
+
+    IEnumerator CheckHealth()
+    {
+            isDying = true;
+            ShowWastedText();
+            Death();
+            yield return new WaitForSeconds(3);
+            HideWastedText();
+            RemoveCashOnDeath();
+            Respawn();
+    }
+    
 }
