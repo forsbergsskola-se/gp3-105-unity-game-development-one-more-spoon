118cca1a25da23e638a0c5b4a078a8f1bb98e033
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-20 13:47:31 +0200

    Add Money Script

---
+++ DeliveryGame/Assets/Scripts/Items.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    private Player player;
+    private void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 2, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            player.AddCash(10);
+        }
+    }
+}
