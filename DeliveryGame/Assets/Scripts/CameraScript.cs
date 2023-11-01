2bbae92ccd41ddda6731cc9fa3804c9beeba7e8a
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-11-01 15:46:42 +0100

    Camera Has A slight Delay

--- DeliveryGame/Assets/Scripts/CameraScript.cs
+++ DeliveryGame/Assets/Scripts/CameraScript.cs
@@ -8,12 +8,16 @@ public class CameraScript : MonoBehaviour
     public GameObject player;
     public Vector3 cameraOffset;
     public Vector3 cameraRotationOffset;
+    public float cameraDelay = 3.96f;
+    
+
     private void LateUpdate()
     {
+        Vector3 targetPosition = player.transform.position + player.transform.TransformDirection(this.cameraOffset);
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraDelay);
         
-        this.transform.position = player.transform.position + player.transform.TransformDirection(this.cameraOffset);
         this.transform.rotation = player.transform.rotation * Quaternion.Euler(this.cameraRotationOffset);
-
+        
 
     }
 }
